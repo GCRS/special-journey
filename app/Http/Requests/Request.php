2755a2cb6265589ae111afee01c6fc78a559534d
Gian Santillan <giansantillan18@gmail.com>
2016-01-31 15:53:43 +0800

    Auth - WIP - still fixing the authentication. Not sure if auth would be successful

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -1,10 +1,19 @@
 <?php
-
 namespace App\Http\Requests;
-
 use Illuminate\Foundation\Http\FormRequest;
-
 abstract class Request extends FormRequest
 {
-    //
+    /**
+     * @param array $errors
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function response(array $errors)
+    {
+      $response = [
+            'message' => 'validation error',
+            'type' => 'validation_error',
+            'errors' => $errors
+        ];
+        return response()->json($response, 422);
+    }
 }
